41779d642817c25ae354aae1a9283ddfcb566a19
László Horváth <bedafoltyn@gmail.com>
2019-06-22 07:52:48 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const {CronJob} = require('cron')
+const db = require('db')
+const rss = require('./lib/rss')
+const weather = require('./lib/weather')
+const rate = require('./lib/rate')
+
+db.init()
+rss.run()
+weather.run()
+rate.run()
+
+new CronJob('* * * * *', function() {
+  rss.run()
+}).start()
+
+new CronJob('*/5 * * * *', function() {
+  weather.run()
+}).start()
+
+new CronJob('0 12 * * *', function () {
+  rate.run()
+}).start()
