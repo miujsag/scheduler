eadcf06ae178fc85dc72b2b93c2a231f4dd26f91
László Horváth <bedafoltyn@gmail.com>
2020-05-03 23:20:11 +0200

    rss scheduler changed to /5 minutes, prettify

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const { CronJob } = require('cron')
-const db = require('db')
-const rss = require('./lib/rss')
-const weather = require('./lib/weather')
-const rate = require('./lib/rate')
+const { CronJob } = require("cron");
+const rss = require("./lib/rss");
+const weather = require("./lib/weather");
+const rate = require("./lib/rate");
 
-db.init()
-rss.run()
-weather.run()
-rate.run()
+rss.run();
+weather.run();
+rate.run();
 
-new CronJob('* * * * *', function () {
-  rss.run()
-}).start()
+new CronJob("*/5 * * * *", function () {
+  rss.run();
+}).start();
 
-new CronJob('*/5 * * * *', function () {
-  weather.run()
-}).start()
+new CronJob("*/5 * * * *", function () {
+  weather.run();
+}).start();
 
-new CronJob('0 12 * * *', function () {
-  rate.run()
-}).start()
+new CronJob("0 12 * * *", function () {
+  rate.run();
+}).start();
