9d9074d99c5571fc809c0a3479a2497bf0c311bb
László Horváth <bedafoltyn@gmail.com>
2020-05-10 01:54:11 +0200

    elasticsearch init bug fixed

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+require("dotenv").config();
 const { CronJob } = require("cron");
+const { createSearchClient, setup } = require("search");
 const rss = require("./lib/rss");
 const weather = require("./lib/weather");
 const rate = require("./lib/rate");
+const client = createSearchClient(process.env.HOST, process.env.LOG);
 
-rss.run();
-weather.run();
-rate.run();
+setup(client)
+  .then(function () {
+    rss.run(client);
+    weather.run();
+    rate.run();
 
-new CronJob("*/5 * * * *", function () {
-  rss.run();
-}).start();
+    new CronJob("*/5 * * * *", function () {
+      rss.run(client);
+    }).start();
 
-new CronJob("*/5 * * * *", function () {
-  weather.run();
-}).start();
+    new CronJob("*/5 * * * *", function () {
+      weather.run();
+    }).start();
 
-new CronJob("0 12 * * *", function () {
-  rate.run();
-}).start();
+    new CronJob("0 12 * * *", function () {
+      rate.run();
+    }).start();
+  })
+  .catch(console.log);
