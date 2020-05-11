2767b1767430ee28782f58db4bfd31190cf8cfa0
László Horváth <bedafoltyn@gmail.com>
2020-05-11 18:27:11 +0200

    frontend updates added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createSearchClient, setup } = require("search");
 const rss = require("./lib/rss");
 const weather = require("./lib/weather");
 const rate = require("./lib/rate");
+const day = require("./lib/day");
 const client = createSearchClient(process.env.HOST, process.env.LOG);
 
 setup(client)
@@ -11,17 +12,22 @@ setup(client)
     rss.run(client);
     weather.run();
     rate.run();
+    day.run();
 
     new CronJob("*/5 * * * *", function () {
       rss.run(client);
     }).start();
 
-    new CronJob("*/5 * * * *", function () {
+    new CronJob("*/30 * * * *", function () {
       weather.run();
     }).start();
 
     new CronJob("0 12 * * *", function () {
       rate.run();
+    }).start();
+
+    new CronJob("0 0 * * *", function () {
+      day.run();
     }).start();
   })
   .catch(console.log);
